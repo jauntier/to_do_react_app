840a8da25564736f5468776784337266a9812967
jauntier <nderituadrian0@gmail.com>
2024-04-23 13:03:10 +0300

    the calculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './Components/Calculator';
+
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-4xl font-bold text-center text-blue-700">Calculator</h1>
+
+      <div className="App h-screen flex justify-center items-center">
+
+        <br></br>
+        <Calculator />
+
+
+      </div>
     </div>
   );
+
+
+
 }
 
 export default App;
